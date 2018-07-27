9eaaca3a0221024accb684c88b24c367bf8dbb43
Tsunemichi Harada <fox10225fox@gmail.com>
2018-07-28 00:46:43 +0900

    fix start_url

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
       options: {
         name: 'GatsbyJS',
         short_name: 'GatsbyJS',
-        start_url: '/futurismo3',
+        start_url: 'https://futurismo.biz/',
         background_color: '#f7f0eb',
         theme_color: '#a2466c',
         display: 'minimal-ui',
