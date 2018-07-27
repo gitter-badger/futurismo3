534372e86d62e5ddde569a0465daf7710d6f6af2
Tsunemichi Harada <fox10225fox@gmail.com>
2018-07-28 00:20:55 +0900

    fix start_url

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
       options: {
         name: 'GatsbyJS',
         short_name: 'GatsbyJS',
-        start_url: '/',
+        start_url: '.',
         background_color: '#f7f0eb',
         theme_color: '#a2466c',
         display: 'minimal-ui',
