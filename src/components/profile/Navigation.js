cd948071d5859b52ac1d7d131fa0e7fe1c4d3d89
Tsunemichi Harada <fox10225fox@gmail.com>
2018-08-03 23:32:50 +0900

    add barger menu

--- src/components/profile/Navigation.js
+++ src/components/profile/Navigation.js
@@ -1,5 +1,30 @@
 import React from 'react'
+import { push as Menu } from 'react-burger-menu'
 
-const Navigation = () => <div />
+const Navigation = () => (
+  <Menu>
+    <a className="menu-item" href="#top">
+      Top
+    </a>
+    <a className="menu-item" href="#about">
+      About
+    </a>
+    <a className="menu-item" href="#works">
+      Works
+    </a>
+    <a className="menu-item" href="#skills">
+      Skills
+    </a>
+    <a className="menu-item" href="#projects">
+      Projects
+    </a>
+    <a className="menu-item" href="/">
+      Blog
+    </a>
+    <a className="menu-item" href="#contact">
+      Contact
+    </a>
+  </Menu>
+)
 
 export default Navigation
