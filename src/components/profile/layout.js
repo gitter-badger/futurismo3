4226d0ec0a6e4140b3bf924a6dd7ffda96c4cf76
Tsunemichi Harada <fox10225fox@gmail.com>
2018-08-04 01:33:25 +0900

    add smallchat

--- src/components/profile/layout.js
+++ src/components/profile/layout.js
@@ -31,6 +31,7 @@ class Template extends React.Component {
       <div className={`body ${this.state.loading}`}>
         <Navigation />
         {children}
+        <script src="https://embed.small.chat/T0GSX24ADG9LLBFV42.js" async />
       </div>
     )
   }
